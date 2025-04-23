fb66bb7e19f5b3a886d9abfb3e356d5befd69323
shintaronogi <shintaro.onogi@bizreach.co.jp>
2025-04-23 15:00:34 +0900

    ４エクササイズくらいやった

--- src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
@@ -1,13 +1,17 @@
 package org.docksidestage.handson.exercise;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import javax.annotation.Resource;
 
 import org.dbflute.cbean.result.ListResultBean;
 import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
-import org.docksidestage.handson.dbflute.exentity.Member;
+import org.docksidestage.handson.dbflute.exbhv.MemberSecurityBhv;
+import org.docksidestage.handson.dbflute.exbhv.PurchaseBhv;
+import org.docksidestage.handson.dbflute.exentity.*;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
 // TODO shiny javadoc by jflute (2025/04/16)
@@ -15,6 +19,12 @@ public class HandsOn03Test extends UnitContainerTestCase {
 
     @Resource
     private MemberBhv memberBhv;
+
+    @Resource
+    private MemberSecurityBhv memberSecurityBhv;
+
+    @Resource
+    private PurchaseBhv purchaseBhv;
 
     /**
      * 会員名称がSで始まる1968年1月1日以前に生まれた会員を検索
@@ -71,7 +81,9 @@ public class HandsOn03Test extends UnitContainerTestCase {
             // MemberStatusが100%存在する理由は？ => (完璧な回答)
             // (NotNullのFKカラムで参照しているから、探しに行けば絶対にする: 物理的に保証されている)
             assertTrue(member.getMemberStatus().isPresent());
-            
+
+            log(member);
+
             // [1on1でのふぉろー]
             // MemberSecurityが100%存在する理由は？ => (いっぱい考えた)
             // (カーディナリティのセカンドレベル、ERDだと黒丸、テーブルコメントにも書いてある)
@@ -86,9 +98,121 @@ public class HandsOn03Test extends UnitContainerTestCase {
             // ただ、物理的な制約はありません。業務的というのは、そういうルールにしているいうことだけなんですね。
             // 細かいですが、これがデータベースプログラミングにおいて、とても重要なんですよね。
             // ぜひ、カージナリティに着目してみてください。
-            
+
             // [1on1でのふぉろー]
             // FK制約を物理的に貼るか貼らないか話、jfluteの意見を説明。
+        });
+    }
+
+    /**
+     * 会員セキュリティ情報のリマインダ質問で2という文字が含まれている会員を検索
+     * 会員セキュリティ情報のデータ自体は要らない
+     * (Actでの検索は本番でも実行されることを想定し、テスト都合でパフォーマンス劣化させないこと)
+     * リマインダ質問に2が含まれていることをアサート
+     * アサートするために別途検索処理を入れても誰も文句は言わない
+     */
+    public void test_3() {
+        // ## Arrange ##
+        String target = "2";
+        // ## Act ##
+        ListResultBean<Member> memberList = memberBhv.selectList(cb -> {
+            cb.query().queryMemberSecurityAsOne().setReminderQuestion_LikeSearch(target, op -> op.likeContain());
+        });
+
+        // ## Assert ##
+        assertHasAnyElement(memberList);
+
+        // 色々なやり方がありそう。脳の記憶をリフレッシュする為に、久々にStream APIを使ってみる。
+        // List<Integer> memberIds = memberList.stream().map(member -> member.getMemberId()).collect(Collectors.toList());
+
+        // Extractという便利なメソッドがあることに気づいた！
+        Set<Integer> memberIds = memberList.extractColumnSet(member -> member.getMemberId());
+        // これもできそう：memberBhv.extractMemberIdList(memberList);
+        ListResultBean<MemberSecurity> memberSecurities = memberSecurityBhv.selectList(cb -> {
+            // TODO jflute "IN" Operatorについて聞きたいです！
+            cb.query().setMemberId_InScope(memberIds);
+        });
+        memberList.forEach(member -> {
+            Integer memberId = member.getMemberId();
+            Optional<String> reminderQuestion = memberSecurities.stream()
+                    .filter(security -> security.getMemberId().equals(memberId))
+                    .findFirst()
+                    .map(security -> security.getReminderQuestion());
+            // Stream APIで検索は直感的に分かりずらい
+            assertTrue(reminderQuestion.isPresent());
+            assertTrue(reminderQuestion.get().contains(target));
+            log("Name: {}, ReminderQuestion: {}", member.getMemberName(), reminderQuestion.get());
+        });
+    }
+
+    /**
+     * 会員ステータスの表示順カラムで会員を並べて検索
+     * 会員ステータスの "表示順" カラムの昇順で並べる
+     * 会員ステータスのデータ自体は要らない
+     * その次には、会員の会員IDの降順で並べる
+     * 会員ステータスのデータが取れていないことをアサート
+     * 会員が会員ステータスごとに固まって並んでいることをアサート (順序は問わない)
+     */
+    public void test_4() {
+        // ## Arrange ##
+        // ## Act ##
+        ListResultBean<Member> memberList = memberBhv.selectList(cb -> {
+            cb.query().queryMemberStatus().addOrderBy_DisplayOrder_Asc();
+            cb.query().addOrderBy_MemberId_Desc();
+        });
+
+        // ## Assert ##
+        assertHasAnyElement(memberList);
+
+        // ここも色々なやり方ありそう
+        // やりやすいアサートとやりにくいやつがある
+        // Lambdaだと並んでることをアサートみないなのはしにくい気がする
+
+        Set<String> memberStatusCodeSet = new HashSet<>();
+        String previousStatusCode = "";
+        for (Member member : memberList) {
+            // 会員ステータスがとれていないことのアサート
+            assertFalse(member.getMemberStatus().isPresent());
+
+            String currentStatusCode = member.getMemberStatusCode();
+            if (currentStatusCode.equals(previousStatusCode)) {
+                assertFalse(memberStatusCodeSet.contains(currentStatusCode));
+                memberStatusCodeSet.add(currentStatusCode);
+            }
+        }
+    }
+
+    /**
+     *生年月日が存在する会員の購入を検索
+     * 会員名称と会員ステータス名称と商品名を取得する(ログ出力)
+     * 購入日時の降順、購入価格の降順、商品IDの昇順、会員IDの昇順で並べる
+     * OrderBy がたくさん追加されていることをログで目視確認すること
+     * 購入に紐づく会員の生年月日が存在することをアサート
+     */
+    public void test_5() {
+        // ## Arrange ##
+        // ## Act ##
+        ListResultBean<Purchase> purchases = purchaseBhv.selectList(cb -> {
+            cb.setupSelect_Member().withMemberStatus();
+            cb.setupSelect_Product();
+            cb.query().addOrderBy_PurchaseDatetime_Desc();
+            cb.query().addOrderBy_PurchasePrice_Desc();
+            cb.query().addOrderBy_ProductId_Asc();
+            cb.query().addOrderBy_MemberId_Asc();
+        });
+
+        assertHasAnyElement(purchases);
+        purchases.forEach(purchase -> {
+            Member member = purchase.getMember().orElseThrow();
+            MemberStatus status = member.getMemberStatus().orElseThrow();
+            Product product = purchase.getProduct().orElseThrow();
+
+            log("MemberName: {}, MemberStatus: {}, ProductName: {}", member.getMemberName(), status.getMemberStatusName(),
+                    purchase.getProductId());
+
+            // 落ちる？
+            // log("birthdate: ", member.getBirthdate());
+            assertNotNull(member.getBirthdate());
         });
     }
 }
